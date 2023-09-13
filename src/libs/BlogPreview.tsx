0d4b9ed510c4dd98f90b34461b2820407610ab8b
masakitami <tamichan.8427@gmail.com>
2023-09-13 20:16:01 +0900

    ⚡️ previewページをSWRに変更

--- src/libs/BlogPreview.tsx
+++ src/libs/BlogPreview.tsx
@@ -13,16 +13,15 @@ export const BlogPreview = () => {
     ([, contentId, draftKey]) => getBlogDetail(contentId, { draftKey }),
   );
 
-  if (error) return <div>エラーが発生しました</div>;
+  if (error) return <div class="text-blue-500">エラーが発生しました</div>;
 
-  if (isLoading) return <div>読み込み中...</div>;
+  if (isLoading) return <div class="text-red-400">読み込み中...</div>;
 
   return (
     <div class="text-white">
-      <h1>{data?.title}kkkk</h1>
+      <h1>{data?.title}</h1>
       <p>{data?.publishedAt ?? data?.createdAt}</p>
       <div dangerouslySetInnerHTML={{ __html: data?.content ?? '' }} />
-
       {isValidating && <div>更新中...</div>}
     </div>
   );
