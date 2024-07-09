6624f22b61cee5d1c5b2f92ebf57e6ae19f08b89
masakitami <tamichan.8427@gmail.com>
2024-07-09 16:02:22 +0900

    画像追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
+import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact({ compat: true })],
+  integrations: [
+    tailwind(),
+    preact({ compat: true }),
+    swup({
+      theme: false, //デフォルトで用意されている遷移アニメーションは使用しない
+      smoothScrolling: false, //スムーズスクロール無効化
+      updateBodyClass: true, //ページ遷移時にbody要素のクラス名を更新する
+      reloadScripts: false, //遷移後にスクリプトを再読み込みさせない
+    }),
+  ],
 });
