c54df1bc59cb78bae6866f64927eca3f2d7f26d3
masakitami <tamichan.8427@gmail.com>
2024-07-19 22:09:10 +0900

    フォントサイズ等を登録

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,46 +1,97 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin'); //プラグインの読み込み
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   future: {
     hoverOnlyWhenSupported: true,
   },
-  fontSize: {
-    xs: ["0.8rem", "1rem"],
-    sm: ["0.89rem", "1.25rem"],
-    base: ["1rem", "1.5rem"],
-    xl: ["1.333rem", "1.75rem"],
-    lg: ["1.14rem", "2rem"],
-    "2xl": ["1.6rem", "2.25rem"],
-    "3xl": ["2rem", "2.5rem"],
-    "4xl": ["2.67rem", "3.25rem"],
-    "5xl": ["4rem", "4.5rem"],
-    "6xl": ["8rem", "8rem"],
-  },
   theme: {
     extend: {
+      fontSize: {
+        jpTextXs: ['0.75rem', '1.6rem'],
+        jpTextSm: ['0.88rem', '1.49rem'],
+        jpTextBase: ['max(1vw,0.8rem)', 'max(150%,1.6rem)'],
+        jpTextLg: ['1.14rem', '1.98rem'],
+        jpTextXl: ['1.33rem', '1.99rem'],
+        jpText2xl: ['1.6rem', '2rem'],
+        jpText3xl: ['2rem', '3rem'],
+        jpText4xl: ['2.66rem', '3.99rem'],
+        jpText5xl: ['4rem', '4.48rem'],
+        enTextBase: ['1rem', '1.5rem'],
+        enTextLg: ['1.14rem', '1.98rem'],
+        enTextXl: ['1.33rem', '1.99rem'],
+        enText2xl: ['1.6rem', '2rem'],
+        enText3xl: ['2rem', '3rem'],
+        enText4xl: ['2.66rem', '3.99rem'],
+        enText5xl: ['max(4vw,2.66rem)', 'max(108%,3.99rem)'],
+        enText7xl: ['max(6vw,4rem)', 'max(108%,4rem)'],
+        enText9xl: ['max(5vw,6rem)', 'max(100%,6.48rem)'],
+      },
+      colors: {
+        'ks-bg': '#EAEAEB',
+        'ks-grey': '#FCFDFE',
+        'ks-black': '#070707',
+        'ks-white': '#FCFDFE',
+      },
       fontFamily: {
-        "roboto-mono": ["Roboto Mono", "monospace"],
+        jp: ['Noto Serif JP', 'serif'],
+        en: ['Cormorant Garamond', 'serif'],
       },
       spacing: {
-        "gutter-x": "max(2.5vw, 1rem)",
-        "gutter-y-0.5": "max(1.25vw, 1rem)",
-        "gutter-y": "max(2.5vw, 2rem)",
-        "gutter-y-2": "max(5vw, 4rem)",
+        'common-gutter-x': 'max(12.6vw, 1.5rem)',
+        'common-gutter-y': 'max(9vw, 5rem)',
       },
       keyframes: {
         marquee: {
           from: {
-            transform: "translateX(0)",
+            transform: 'translateX(0)',
           },
           to: {
-            transform: "translateX(var(--translate-x,-50%))",
+            transform: 'translateX(var(--translate-x,-50%))',
           },
         },
       },
       zIndex: {
-        header: "100",
+        header: '100',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({
+      addBase,
+      addComponents,
+      addUtilities,
+      addVariant,
+      theme,
+      matchUtilities,
+    }) {
+      // スマホではホバー禁止
+      addVariant(
+        'hover',
+        '@media(hover:hover){ &:where(:any-link, :enabled, summary):hover }',
+      );
+      // デフォルトCSS
+      addBase({
+        body: {
+          'font-family': "'Noto Serif JP', 'Cormorant Garamond', 'serif'",
+          'font-weight': '300',
+          'background-color': theme('colors.ks-bg'),
+          color: theme('colors.ks-black'),
+        },
+      });
+      addComponents({});
+      addUtilities({
+        '.content-auto': {
+          contentVisibility: 'auto',
+        },
+      });
+      matchUtilities({
+        intrincSize: (value) => {
+          return {
+            'contain-intrinsic-size': value,
+          };
+        },
+      });
+    }),
+  ],
 };
